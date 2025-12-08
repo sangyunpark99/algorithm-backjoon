0ae09fcc4b9ab784b6b1e847b8803713ff77acf6
sangyunpark99 <psangyun5108@gmail.com>
2025-12-08 20:56:10 +0900

    DP - 게임 개발

--- src/main/java/org/hello/문제풀이/DP/게임개발/Main.java
+++ src/main/java/org/hello/문제풀이/DP/게임개발/Main.java
@@ -3,33 +3,62 @@ package org.hello.문제풀이.DP.게임개발;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 public class Main {
 
     private static int N;
-    private static int[] costs;
-    private static List<int[]> build = new ArrayList<>();
+    private static int[] time;
+    private static List<List<Integer>> build = new ArrayList<>(); // 해당 idx가 선행인 모든 건물들
+    private static int[] buildCnt;
+    private static int[] cost;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
 
-        costs = new int[N + 1];
+        time = new int[N + 1];
+        buildCnt = new int[N + 1];
+        cost = new int[N + 1];
+
         for(int i = 0; i <= N; i++) {
-            build.add(new int[]{});
+            build.add(new ArrayList<>());
         }
 
         for(int i = 0; i < N; i++) {
             int[] arr = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
             int cost = arr[0];
-            costs[i + 1] = cost;
-            int[] bfBuild = Arrays.copyOfRange(arr, 1, arr.length - 1);
-            build.set(i + 1, bfBuild);
+            int number = i + 1;
+            time[number] = cost;
+            int[] bfBuild = Arrays.copyOfRange(arr, 1, arr.length - 1); // 이전에 지어야할 건물
+            buildCnt[number] = bfBuild.length;
+            for(int j = 0; j < bfBuild.length; j++) {
+                build.get(bfBuild[j]).add(number);
+            }
         }
 
-        
+        Queue<Integer> queue = new ArrayDeque<>(); // 지어진 건물
+
+        for(int i = 1; i <= N; i++) {
+            if(buildCnt[i] == 0) {
+                cost[i] = time[i];
+                queue.offer(i);
+            }
+        }
+
+        while(!queue.isEmpty()) {
+            int cur = queue.poll();
+            for(int next : build.get(cur)) {
+                cost[next] = Math.max(cost[next], cost[cur] + time[next]);
+                if(--buildCnt[next] == 0) {
+                    queue.offer(next);
+                }
+            }
+        }
+
+
+        for(int i = 1; i <= N; i++) {
+            System.out.println(cost[i]);
+        }
     }
 }
