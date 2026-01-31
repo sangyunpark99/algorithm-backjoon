6fcc241378fc213025ed281802daf862269e82d6
sangyunpark99 <psangyun5108@gmail.com>
2026-01-31 16:19:42 +0900

    DP - 기업 투자

--- src/main/java/org/hello/문제풀이/냅색/기업투자/Main.java
+++ src/main/java/org/hello/문제풀이/냅색/기업투자/Main.java
@@ -10,7 +10,6 @@ public class Main {
     private static int M;
     private static int N;
     private static int[][] profits;
-    private static int[][] dp;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -31,36 +30,39 @@ public class Main {
             }
         } // 기업별 정해진 액수 투자시 얻을 수 있는 수익
 
-        dp = new int[M + 1][N + 1];
+        int[][] choice = new int[M + 1][N + 1];
+        int[][] dp = new int[M + 1][N + 1];
 
-        int[][] choice = new int[M + 1][N + 1]; // dp[i][j]가 최대일 때 i번 기업에 투자한 금액 k
-
-        for(int i = 1; i <= M; i++) {
-            for(int j = 1; j <= N; j++) {
+        for(int i = 1; i <= M; i++) { // 기업 선택
+            for(int j = 1; j <= N; j++) { // 투자할 총 금액
                 dp[i][j] = 0;
-                for(int k = 0; k <= j; k++) {
-                    int value = dp[i - 1][j - k] + profits[i][k];
-                    if(value > dp[i][j]) { // 최대인 경우
+                for(int k = 0; k <= j; k++) { // i번째 기업에 K원 투자
+                    int value = dp[i - 1][j - k] + profits[i][k]; // 1 ~ i - 1
+                    if(value > dp[i][j]) {
                         dp[i][j] = value;
-                        choice[i][j] = k; // i번 기업에 준 돈 K
-                    }
+                        choice[i][j] = k; // 총 금액 j를 갖고 1~i 번째 기업까지 고려했을때, i번째 기업에 투자한 금액
+                     }
                 }
             }
         }
 
         System.out.println(dp[M][N]);
 
+        int money = N;
         int[] invest = new int[M + 1];
 
-        int money = N;
         for(int i = M; i >= 1; i--) {
-            int k = choice[i][money];
-            invest[i] = k;
-            money -= k;
+            int value = choice[i][money];
+            invest[i] = value;
+            money -= value;
         }
 
         StringBuilder sb = new StringBuilder();
-        for(int i = 1; i <= M; i++) sb.append(invest[i]).append(" ");
+
+        for(int i = 1; i <= M; i++) {
+            sb.append(invest[i]).append(" ");
+        }
+
         System.out.println(sb);
     }
 }
